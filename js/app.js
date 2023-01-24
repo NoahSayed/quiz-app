9b27548359cbdd780aebfdbef3b9aa15feaef8a3
Noah <noahsayeddb@gmail.com>
2023-01-24 19:54:58 +0400

    linked Question's innerhtml to  Array

--- js/app.js
+++ js/app.js
@@ -85,3 +85,13 @@ allInputs.forEach(
 )
 }
 
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+    </div>
+    
+`
+}
+loadQuestion(index);
